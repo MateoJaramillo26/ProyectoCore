53ed532a80e99a10bc40333c85a3caff90648e7d
MateoJaramillo <mateo.jaramillo.costa@udla.edu.ec>
2025-05-08 22:20:38 -0500

    Creacion de los archivos necesarios para el deploy en vercel

--- OrientacionVocacional/OrientacionVocacional/settings.py
+++ OrientacionVocacional/OrientacionVocacional/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t+g2j^qvs%1u25=6!f5lnx#&mw!11l+^8+0tq%dg848s&z6jnz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
